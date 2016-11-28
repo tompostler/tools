e06b745a101d5af12a123559772da7049e69d64d
Tom Postler <tom@postler.me>
2016-11-27 18:26:58 -0600

    Started hashing

---
+++ Unlimitedinf.Tools/Unlimitedinf.Tools/Hashing/Blockhash.cs
@@ -0,0 +1,41 @@
+﻿/* Perceptual image hash calculation tool based on algorithm descibed in
+ * Block Mean Value Based Image Perceptual Hashing by Bian Yang, Fan Gu and Xiamu Niu
+ *
+ * Copyright 2014 Commons Machinery http://commonsmachinery.se/
+ * Distributed under an MIT license, please see LICENSE in the top dir.
+ */
+// Source drawn from https://github.com/commonsmachinery/blockhash/blob/master/blockhash.c on 2016-11-27
+// Modified under the MIT license by Tom Postler to work with C#
+
+namespace Unlimitedinf.Tools.Hashing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Perceptual image hash calculation tool based on algorithm descibed in 
+    /// Block Mean Value Based Image Perceptual Hashing by Bian Yang, Fan Gu and Xiamu Niu.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Blockhash")]
+    public sealed class Blockhash : HashAlgorithm
+    {
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override byte[] HashFinal()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Unlimitedinf.Tools/Unlimitedinf.Tools/Hashing/Hasher.cs
@@ -0,0 +1,91 @@
+﻿namespace Unlimitedinf.Tools.Hashing
+{
+    using System;
+    using System.Security.Cryptography;
+
+    /// <summary>
+    /// A wrapper for common hashing functions to operate on streams or files.
+    /// </summary>
+    public sealed class Hasher
+    {
+        /// <summary>
+        /// Helper to make it obvious which hash algorithm we're looking for.
+        /// </summary>
+        public enum Algorithm
+        {
+            MD5,
+            Crc32,
+            SHA1,
+            SHA256,
+            SHA512,
+            Blockhash   // http://blockhash.io/
+        }
+
+        private Algorithm algorithm;
+        private HashAlgorithm hashAlgorithm;
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        /// <param name="algorithm"></param>
+        public Hasher(Algorithm algorithm)
+        {
+            this.algorithm = algorithm;
+            this.ResetAlgorithm();
+        }
+
+        /// <summary>
+        /// Ctor that by default discards any setup you may have done to your choice of algorithm.
+        /// </summary>
+        /// <param name="algorithm"></param>
+        /// <param name="preserve"></param>
+        public Hasher(HashAlgorithm algorithm, bool preserve = false)
+        {
+            if (algorithm is MD5)
+                this.algorithm = Algorithm.MD5;
+            else if (algorithm is Crc32)
+                this.algorithm = Algorithm.Crc32;
+            else if (algorithm is SHA1)
+                this.algorithm = Algorithm.SHA1;
+            else if (algorithm is SHA256)
+                this.algorithm = Algorithm.SHA256;
+            else if (algorithm is SHA512)
+                this.algorithm = Algorithm.SHA512;
+            else if (algorithm is Blockhash)
+                this.algorithm = Algorithm.Blockhash;
+
+            if (preserve)
+                this.hashAlgorithm = algorithm;
+            else
+                this.ResetAlgorithm();
+        }
+
+        /// <summary>
+        /// Resets the base algorithm to be fresh and unaffected.
+        /// </summary>
+        private void ResetAlgorithm()
+        {
+            switch (this.algorithm)
+            {
+                case Algorithm.Blockhash:
+                    this.hashAlgorithm = Blockhash.Create();
+                    break;
+                case Algorithm.Crc32:
+                    this.hashAlgorithm = Crc32.Create();
+                    break;
+                case Algorithm.MD5:
+                    this.hashAlgorithm = MD5.Create();
+                    break;
+                case Algorithm.SHA1:
+                    this.hashAlgorithm = SHA1.Create();
+                    break;
+                case Algorithm.SHA256:
+                    this.hashAlgorithm = SHA256.Create();
+                    break;
+                case Algorithm.SHA512:
+                    this.hashAlgorithm = SHA512.Create();
+                    break;
+            }
+        }
+    }
+}
